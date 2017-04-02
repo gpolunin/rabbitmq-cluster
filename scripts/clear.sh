235c9f408746ac0844f5b4bdb7217c4049dd451a
george <polunin@targetprocess.com>
2017-04-02 23:54:29 +0300

    add etcd

--- scripts/clear.sh
+++ scripts/clear.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kubectl delete secrets,services,ingresses,persistentvolumeclaims,persistentvolumes,daemonsets,statefulsets,deployments,replicasets,pods --all --namespace=cluster
+kubectl delete secrets,services, ingresses,persistentvolumeclaims,persistentvolumes,daemonsets,statefulsets,deployments,replicasets,pods --all --namespace=cluster
